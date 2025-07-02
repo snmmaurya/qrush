f4b5182d2dc4ba8cc45dce4bc7ca7105bb56f9eb
Sandeep Maurya <inxmaurya@Mac.lan>
2025-07-02 23:15:26 +0530

    redis tls and non tls connection

--- src/redis_pool.rs
+++ src/redis_pool.rs
@@ -2,6 +2,9 @@ use redis::{aio::MultiplexedConnection, Client};
 use crate::config::get_redis_url;
 
 pub async fn get_redis_conn() -> redis::RedisResult<MultiplexedConnection> {
-    let client = Client::open(get_redis_url())?;
+    let redis_url = get_redis_url();
+
+    // Client::open will auto-handle rediss:// if TLS feature is enabled
+    let client = Client::open(redis_url)?;
     client.get_multiplexed_async_connection().await
 }
