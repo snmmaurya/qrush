b351622acb444762c3bc7f8d1b02acd5c325ae65
Sandeep Maurya <inxmaurya@gmail.com>
2025-08-28 12:55:48 +0530

    in metrics delayed jobs missing route added

--- src/routes/metrics_route.rs
+++ src/routes/metrics_route.rs
@@ -4,6 +4,7 @@ use actix_web::{web, HttpResponse};
 use crate::services::{
     metrics_service::{
         render_metrics,
+        render_delayed_jobs,
         render_metrics_for_queue,
         render_dead_jobs,
         render_scheduled_jobs,
@@ -30,6 +31,7 @@ pub fn qrush_metrics_routes(cfg: &mut web::ServiceConfig) {
             .route("", web::get().to(render_metrics))
             .route("/queues/{queue}", web::get().to(render_metrics_for_queue))
             .route("/extras/dead", web::get().to(render_dead_jobs))
+            .route("/extras/delayed", web::get().to(render_delayed_jobs))
             .route("/extras/scheduled", web::get().to(render_scheduled_jobs))
             .route("/queues/{queue}/export", web::get().to(export_queue_csv))
             .route("/extras/summary", web::get().to(get_metrics_summary))
